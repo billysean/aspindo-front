8d32b98b868143bc21d598fe950741c1587a9490
Billy Sean TJ <billysean.tj@gmail.com>
2021-04-24 14:27:57 +0700

    blog layout update

--- src/pages/en/Blog/index.vue
+++ src/pages/en/Blog/index.vue
@@ -1,35 +1,252 @@
 <template>
   <Layout>
-    <section>
-      <div class="h-10">
-        <div class="image">
-          <img
-            class="absolute sm:h-auto w-screen"
-            style="z-index: -1"
-            src="../../../asset/newspaper.jpg"
-            alt=""
-          />
-        </div>
-      </div>
-      <div
-        class="hero container md:container-inner mx-auto flex flex-col sm:flex-row justify-between py-16"
-      >
-        <div
-          class="text-4xl font-bold w-full sm:w-3/5 text-center sm:text-left"
-        >
-          <div class="uppercase text-white">Blog</div>
+    <section class="text-gray-600 body-font">
+      <div class="container px-5 py-24 mx-auto">
+        <div class="flex flex-wrap -m-4">
+          <div class="p-4 lg:w-1/3">
+            <div
+              class="h-full bg-gray-100 bg-opacity-75 px-8 pt-16 pb-24 rounded-lg overflow-hidden text-center relative"
+            >
+              <h2
+                class="tracking-widest text-xs title-font font-medium text-gray-400 mb-1 uppercase"
+              >
+                Asphalt
+              </h2>
+              <h1
+                class="title-font sm:text-2xl text-xl font-medium text-gray-900 mb-3"
+              >
+                Raclette Blueberry Nextious Level
+              </h1>
+              <p class="leading-relaxed mb-3">
+                Photo booth fam kinfolk cold-pressed sriracha leggings jianbing
+                microdosing tousled waistcoat.
+              </p>
+              
+              <div
+                class="text-center mt-2 leading-none flex justify-center absolute bottom-0 left-0 w-full py-4"
+              >
+                <a class="text-indigo-500 inline-flex items-center"
+                >Learn More
+                <svg
+                  class="w-4 h-4 ml-2"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  stroke-width="2"
+                  fill="none"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                >
+                  <path d="M5 12h14"></path>
+                  <path d="M12 5l7 7-7 7"></path>
+                </svg>
+              </a>
+              </div>
+            </div>
+          </div>
+          <div class="p-4 lg:w-1/3">
+            <div
+              class="h-full bg-gray-100 bg-opacity-75 px-8 pt-16 pb-24 rounded-lg overflow-hidden text-center relative"
+            >
+              <h2
+                class="tracking-widest text-xs title-font font-medium text-gray-400 mb-1 uppercase"
+              >
+                bbm
+              </h2>
+              <h1
+                class="title-font sm:text-2xl text-xl font-medium text-gray-900 mb-3"
+              >
+                Ennui Snackwave Thundercats
+              </h1>
+              <p class="leading-relaxed mb-3">
+                Photo booth fam kinfolk cold-pressed sriracha leggings jianbing
+                microdosing tousled waistcoat.
+              </p>
+              
+              <div
+                class="text-center mt-2 leading-none flex justify-center absolute bottom-0 left-0 w-full py-4"
+              >
+                <a class="text-indigo-500 inline-flex items-center"
+                >Learn More
+                <svg
+                  class="w-4 h-4 ml-2"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  stroke-width="2"
+                  fill="none"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                >
+                  <path d="M5 12h14"></path>
+                  <path d="M12 5l7 7-7 7"></path>
+                </svg>
+              </a>
+              </div>
+            </div>
+          </div>
+          <div class="p-4 lg:w-1/3">
+            <div
+              class="h-full bg-gray-100 bg-opacity-75 px-8 pt-16 pb-24 rounded-lg overflow-hidden text-center relative"
+            >
+              <h2
+                class="tracking-widest text-xs title-font font-medium text-gray-400 mb-1 uppercase"
+              >
+                LPG
+              </h2>
+              <h1
+                class="title-font sm:text-2xl text-xl font-medium text-gray-900 mb-3"
+              >
+                Selvage Poke Waistcoat Godard
+              </h1>
+              <p class="leading-relaxed mb-3">
+                Photo booth fam kinfolk cold-pressed sriracha leggings jianbing
+                microdosing tousled waistcoat.
+              </p>
+              
+              <div
+                class="text-center mt-2 leading-none flex justify-center absolute bottom-0 left-0 w-full py-4"
+              >
+                <a class="text-indigo-500 inline-flex items-center"
+                >Learn More
+                <svg
+                  class="w-4 h-4 ml-2"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  stroke-width="2"
+                  fill="none"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                >
+                  <path d="M5 12h14"></path>
+                  <path d="M12 5l7 7-7 7"></path>
+                </svg>
+              </a>
+              </div>
+            </div>
+          </div>
+
+          <div class="p-4 lg:w-1/3">
+            <div
+              class="h-full bg-gray-100 bg-opacity-75 px-8 pt-16 pb-24 rounded-lg overflow-hidden text-center relative"
+            >
+              <h2
+                class="tracking-widest text-xs title-font font-medium text-gray-400 mb-1 uppercase"
+              >
+                Asphalt
+              </h2>
+              <h1
+                class="title-font sm:text-2xl text-xl font-medium text-gray-900 mb-3"
+              >
+                Raclette Blueberry Nextious Level
+              </h1>
+              <p class="leading-relaxed mb-3">
+                Photo booth fam kinfolk cold-pressed sriracha leggings jianbing
+                microdosing tousled waistcoat.
+              </p>
+              
+              <div
+                class="text-center mt-2 leading-none flex justify-center absolute bottom-0 left-0 w-full py-4"
+              >
+                <a class="text-indigo-500 inline-flex items-center"
+                >Learn More
+                <svg
+                  class="w-4 h-4 ml-2"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  stroke-width="2"
+                  fill="none"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                >
+                  <path d="M5 12h14"></path>
+                  <path d="M12 5l7 7-7 7"></path>
+                </svg>
+              </a>
+              </div>
+            </div>
+          </div>
+          <div class="p-4 lg:w-1/3">
+            <div
+              class="h-full bg-gray-100 bg-opacity-75 px-8 pt-16 pb-24 rounded-lg overflow-hidden text-center relative"
+            >
+              <h2
+                class="tracking-widest text-xs title-font font-medium text-gray-400 mb-1 uppercase"
+              >
+                Seafood
+              </h2>
+              <h1
+                class="title-font sm:text-2xl text-xl font-medium text-gray-900 mb-3"
+              >
+                Raclette Blueberry Nextious Level
+              </h1>
+              <p class="leading-relaxed mb-3">
+                Photo booth fam kinfolk cold-pressed sriracha leggings jianbing
+                microdosing tousled waistcoat.
+              </p>
+              
+              <div
+                class="text-center mt-2 leading-none flex justify-center absolute bottom-0 left-0 w-full py-4"
+              >
+                <a class="text-indigo-500 inline-flex items-center"
+                >Learn More
+                <svg
+                  class="w-4 h-4 ml-2"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  stroke-width="2"
+                  fill="none"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                >
+                  <path d="M5 12h14"></path>
+                  <path d="M12 5l7 7-7 7"></path>
+                </svg>
+              </a>
+              </div>
+            </div>
+          </div>
+          <div class="p-4 lg:w-1/3">
+            <div
+              class="h-full bg-gray-100 bg-opacity-75 px-8 pt-16 pb-24 rounded-lg overflow-hidden text-center relative"
+            >
+              <h2
+                class="tracking-widest text-xs title-font font-medium text-gray-400 mb-1 uppercase"
+              >
+                Seafood
+              </h2>
+              <h1
+                class="title-font sm:text-2xl text-xl font-medium text-gray-900 mb-3"
+              >
+                Raclette Blueberry Nextious Level
+              </h1>
+              <p class="leading-relaxed mb-3">
+                Photo booth fam kinfolk cold-pressed sriracha leggings jianbing
+                microdosing tousled waistcoat.
+              </p>
+              
+              <div
+                class="text-center mt-2 leading-none flex justify-center absolute bottom-0 left-0 w-full py-4"
+              >
+                <a class="text-indigo-500 inline-flex items-center"
+                >Learn More
+                <svg
+                  class="w-4 h-4 ml-2"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  stroke-width="2"
+                  fill="none"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                >
+                  <path d="M5 12h14"></path>
+                  <path d="M12 5l7 7-7 7"></path>
+                </svg>
+              </a>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </section>
-    <h1 class="text-xl mb-2">Blog site is on the way! Please wait patiently</h1>
-
-    <div class="space-y-4">
-      <BlogFeedPost
-        v-for="edge in $page.allPost.edges"
-        :key="edge.node.id"
-        :post="edge.node"
-      />
-    </div>
   </Layout>
 </template>
 
